f4e185f5602cf7de8ec6a1b0d719141b06b4e96d
rnidea2code <rn.idea2code@gmail.com>
2021-01-06 00:41:17 +0530

    added changes on navbar

--- src/pages/Dashbord.jsx
+++ src/pages/Dashbord.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default () => {
   return (
     <div className="">
-      {/* <img src="https://i.imgur.com/itts48F.png"/> */}
-      <div style={{ height: "100vh", background: "red" }}></div>
+      <img src="https://i.imgur.com/itts48F.png"/>
+      {/* <div style={{ height: "100vh", background: "red" }}></div> */}
     </div>
   );
 };
