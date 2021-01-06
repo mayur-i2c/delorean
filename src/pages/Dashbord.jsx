ce219f7b0c4a743f02ad65bb68d58ffe162cfc62
rnidea2code <rn.idea2code@gmail.com>
2021-01-06 16:45:33 +0530

    responsive fix dashboard issue

--- src/pages/Dashbord.jsx
+++ src/pages/Dashbord.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Dashboard() {
   return (
-    <div style={{ background: "red", height: "95vh" }}>
-      {/* <img src="https://i.imgur.com/itts48F.png" /> */}
-      {/* <h1>hello</h1> */}
+    // style={{ background: "red", height: "95vh" }}
+    <div>
+      <img src="https://i.imgur.com/itts48F.png" />
     </div>
   );
 }
