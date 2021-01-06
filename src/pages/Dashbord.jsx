f45b94e530442d0395a3406954221f2f97f0af9e
rnidea2code <rn.idea2code@gmail.com>
2021-01-06 17:51:10 +0530

    fix hover issue

--- src/pages/Dashbord.jsx
+++ src/pages/Dashbord.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Dashboard() {
   return (
     // style={{ background: "red", height: "95vh", minWidth: "100vh" }}
-    <div>
+    <div style={{ background: "red", }}>
       <img src="https://i.imgur.com/itts48F.png" />
     </div>
   );
